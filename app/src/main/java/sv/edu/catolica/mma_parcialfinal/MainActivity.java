faaf52ab4230ce47df8767b9271125e53674642f
MarcoEsquivel1 <marco.esquivel@catolica.edu.sv>
2022-05-25 11:46:35 -0600

    first commit

---
+++ app/src/main/java/sv/edu/catolica/mma_parcialfinal/MainActivity.java
@@ -0,0 +1,14 @@
+package sv.edu.catolica.mma_parcialfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
